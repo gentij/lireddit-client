d94e8c12c5278bb4870e287ee6e40f22e2554ddb
Gentrit Jashari <gentijashari5@gmail.com>
2022-02-06 13:13:31 +0100

    implement server side renderring on home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Box, Button, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 
 interface NavbarProps {}
 export const Navbar: React.FC<NavbarProps> = ({}) => {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
 
   let body = null;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { withUrqlClient } from "next-urql";
 import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
+  const [{ data }] = usePostsQuery();
   return (
     <>
       <Navbar />
+      {!data
+        ? null
+        : data.posts.map((post) => <div key={post.id}>{post.title}</div>)}
       <div>haha</div>
     </>
   );
 };
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
